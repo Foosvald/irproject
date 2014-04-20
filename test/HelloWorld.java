0749325932e315bfe1897523746263678a3e748a
matselexell <matslexell88@gmail.com>
2014-04-20 11:04:38 +0200

    testar bara om jag kan lägga till filer

---
+++ test/HelloWorld.java
@@ -0,0 +1,12 @@
+package test;
+
+public class HelloWorld {
+	
+	public HelloWorld() {
+		System.out.println("Hello world!");
+	}
+
+	public static void main(String[] args) {
+		new HelloWorld();
+	}
+}

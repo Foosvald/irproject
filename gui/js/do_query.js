b5e70130c909e1eed1eda1f42c8f45d72cae8069
Osvald Ivarsson <osvald@osvald.org>
2014-05-07 13:37:42 +0200

    rensade lite kod

--- gui/js/do_query.js
+++ gui/js/do_query.js
@@ -1,15 +1,21 @@
-$(document).ready(function () {
-	$( '#former' ).submit(function( e ) {
-		var post_data =$('#query').val();
+$(document).ready(function() {
+	$('#former').submit(function(e) {
+		var post_data = $('#query').val();
 		$.ajax({
 			type: 'GET',
 			url: 'http://localhost:8983/solr/collection1/select',
 			dataType: 'jsonp',
-			data: {'q': post_data, 'wt':'json','indent' : 'true', 'json.wrf': 'display_data'},
-			beforeSend : function (){
+			data: {
+				'q': post_data,
+				'wt': 'json',
+				'indent': 'true',
+				'json.wrf': 'display_data'
+			},
+			beforeSend: function() {
 				empty_table();
 			},
-			success: function(data) { 
+			success: function(data) {
+				console.log(data)
 				alert('success');
 			}
 		});
@@ -17,44 +23,34 @@ $(document).ready(function () {
 	});
 });
 
-function display_data(data){
+function display_data(data) {
+	console.log(data);
 	var counter = 0;
-	$.each(data, function(key, value){
-    $.each(value, function(key, value){
-				if(key == 'docs'){
-				  $.each(value, function(key, value){
-						counter++;
-						$('table').append('<tr>');
-						var current =  $( "tr:last" );
-						current.append('<td style="color:#FFF">' + counter + '</td>');
-						$.each(value, function(key, value){
-							if(key == 'name'){
-								
-								current.append('<td><a href="http://commons.wikimedia.org/wiki/File:'+ add_underscore(value) +'" style="color:#FFF">' + remove_special_chars(strip_extension(value)) + '</a></td>');
-							}
-							else if(key == 'description'){
-								current.append('<td style="color:#FFF">' + remove_special_chars(value) +'</td>');
-							}
-						});
-					});
-				}
-    });
-	});
+	var docs = data.response.docs;
+	for (var i = 0; i < docs.length; i++) {
+		var doc = docs[i];
+
+		$('table').append('<tr>');
+		var current = $("tr:last");
+		current.append('<td style="color:#FFF">' + (i + 1) + '</td>');
+		current.append('<td><a href="http://commons.wikimedia.org/wiki/File:' + add_underscore(doc.name) 
+			+ '" style="color:#FFF">' + remove_special_chars(strip_extension(doc.name)) + '</a></td>');
+		current.append('<td style="color:#FFF">' + remove_special_chars(doc.description) + '</td>');
+	}
 }
 
-function empty_table(){
+function empty_table() {
 	$('table tr:not(:first)').remove();
 }
 
-function add_underscore(string){
+function add_underscore(string) {
 	return string.split(' ').join('_');
 }
 
-function remove_special_chars(string){
+function remove_special_chars(string) {
 	return string.replace(/[_\s-.]/g, ' ');
 }
 
-function strip_extension(string){
+function strip_extension(string) {
 	return string.substr(0, string.lastIndexOf('.')) || string
 }
-
